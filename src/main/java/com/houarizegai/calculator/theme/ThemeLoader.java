49dfd09086383083e1189b7d5e6469c0cf2d2f82
OleksiiZhurko <alexey.zhurko0@gmail.com>
2023-03-21 20:53:22 +0200

    Simplified color reading from theme config

--- src/main/java/com/houarizegai/calculator/theme/ThemeLoader.java
+++ src/main/java/com/houarizegai/calculator/theme/ThemeLoader.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.houarizegai.calculator.theme.properties.Theme;
 import com.houarizegai.calculator.theme.properties.ThemeList;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
 
 public class ThemeLoader {
+
+    private static final String THEME_RESOURCE_FILE = "theme.yaml";
 
     private ThemeLoader() {
         throw new AssertionError("Constructor is not allowed");
@@ -19,8 +20,10 @@ public class ThemeLoader {
     public static Map<String, Theme> loadThemes() {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         mapper.findAndRegisterModules();
-        try {
-            ThemeList themeList = mapper.readValue(new File("src/main/resources/application.yaml"), ThemeList.class);
+        try (var fileStream = ThemeLoader.class.getClassLoader()
+            .getResourceAsStream(THEME_RESOURCE_FILE)
+        ) {
+            ThemeList themeList = mapper.readValue(fileStream, ThemeList.class);
             return themeList.getThemesAsMap();
         } catch (IOException e) {
             return Collections.emptyMap();
