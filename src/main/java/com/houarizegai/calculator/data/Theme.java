9182efaa29a8f88ffc4d9e1746f09ab2079640ef
OleksiiZhurko <alexey.zhurko0@gmail.com>
2023-03-21 21:32:37 +0200

    Added Builder

--- src/main/java/com/houarizegai/calculator/data/Theme.java
+++ src/main/java/com/houarizegai/calculator/data/Theme.java
@@ -10,6 +10,16 @@ public class Theme {
     private String numbersBackground;
     private String btnEqualBackground;
 
+    private Theme(Builder builder) {
+        this.name = builder.name;
+        this.applicationBackground = builder.applicationBackground;
+        this.textColor = builder.textColor;
+        this.btnEqualTextColor = builder.btnEqualTextColor;
+        this.operatorBackground = builder.operatorBackground;
+        this.numbersBackground = builder.numbersBackground;
+        this.btnEqualBackground = builder.btnEqualBackground;
+    }
+
     public String getName() {
         return name;
     }
@@ -64,5 +74,54 @@ public class Theme {
 
     public void setBtnEqualBackground(String btnEqualBackground) {
         this.btnEqualBackground = btnEqualBackground;
+    }
+
+    public static class Builder {
+        private String name;
+        private String applicationBackground;
+        private String textColor;
+        private String btnEqualTextColor;
+        private String operatorBackground;
+        private String numbersBackground;
+        private String btnEqualBackground;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setApplicationBackground(String applicationBackground) {
+            this.applicationBackground = applicationBackground;
+            return this;
+        }
+
+        public Builder setTextColor(String textColor) {
+            this.textColor = textColor;
+            return this;
+        }
+
+        public Builder setBtnEqualTextColor(String btnEqualTextColor) {
+            this.btnEqualTextColor = btnEqualTextColor;
+            return this;
+        }
+
+        public Builder setOperatorBackground(String operatorBackground) {
+            this.operatorBackground = operatorBackground;
+            return this;
+        }
+
+        public Builder setNumbersBackground(String numbersBackground) {
+            this.numbersBackground = numbersBackground;
+            return this;
+        }
+
+        public Builder setBtnEqualBackground(String btnEqualBackground) {
+            this.btnEqualBackground = btnEqualBackground;
+            return this;
+        }
+
+        public Theme build() {
+            return new Theme(this);
+        }
     }
 }
